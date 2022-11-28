23c4e21616734996375bd5692a08ce975f024a14
Anuj Rao <anujr@tekion.com>
2022-11-28 17:40:34 +0530

    Add exports for Sudoku App
    this branch will be used as a dependency in sudoku app and the app will use
    Svg elements from this repository instead of directly from react-native-svg dependency
    to reduce the bundle size

---
+++ index.js
@@ -0,0 +1,9 @@
+import G from 'react-native-svg/src/elements/G'
+import Svg from 'react-native-svg/src/elements/Svg'
+import Path from 'react-native-svg/src/elements/Path'
+
+export {
+  G,
+  Svg,
+  Path
+}
